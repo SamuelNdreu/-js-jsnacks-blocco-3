bd1c0882ec647ee3f78795af258a6a2b4af9ec5f
SamuelNdreu <samwellsoficcial@gmail.com>
2022-12-01 11:21:25 +0100

    created snack_4 folder

---
+++ snack_4/js/script.js
@@ -0,0 +1,8 @@
+/**
+ *
+ * Data una lista di 20 numeri diversi.
+ * generiamo una lista secondaria di
+ * 10 numeri presi randomicamente dalla lista, diversi tra loro.
+ *
+ */
+ const startingList = [ 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20];
